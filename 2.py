a6447aa5465ce138d58e1b5e6208a5d235398883
elhajuojy <souirimehdi311@gmail.com>
2021-07-10 14:19:13 +0100

    add new soulation for hassan bahi exercise 2 3

---
+++ 2.py
@@ -0,0 +1,3 @@
+datenais= int(input(" please entre your birthday"))
+age = 2021-datenais
+print(f"your age is {age}")
---
+++ 3.py
@@ -0,0 +1,5 @@
+longueur=float(input("donner la longueur "))
+largeur=float(input("donner la largeur"))
+surface=longueur*largeur
+perimetre=(longueur+largeur)*2
+print(f"surface est {surface} est les perimetre est {perimetre}")
