6dbe12d9275b9125d8bf0272b432eb3db92ede47
elhajuojy <souirimehdi311@gmail.com>
2021-07-12 15:25:48 +0100

    6

---
+++ 6.py
@@ -0,0 +1,10 @@
+N1=float(input(" please entre your garde N1 :"))
+N2=float(input(" please entre your garde N2 :"))
+N3=float(input(" please entre your garde N3 :"))
+N4=float(input(" please entre your garde N4 :"))
+N5=float(input(" please entre your garde N5 :"))
+
+somme=N1+N2+N3+N4+N5
+Moyenne=somme/5
+print("la somme est ",somme)
+print("la Moyenne est ",Moyenne)
