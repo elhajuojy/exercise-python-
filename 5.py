53a7606a0c6ee274c5dc2d091963636ad37f498f
elhajuojy <souirimehdi311@gmail.com>
2021-07-12 15:18:09 +0100

    5

---
+++ 5.py
@@ -0,0 +1,6 @@
+a=float(input("donner une nomber"))
+b=float(input("please entre b"))
+somme=a+b
+proudit=a*b
+difference=a-b
+print(f"la somme est {somme}\nla proudit est {proudit}\nla differnce de a et b est {difference}")
