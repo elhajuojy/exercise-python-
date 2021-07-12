77ff6af6c0f90063b6c16278eb8f990606bcb45b
elhajuojy <souirimehdi311@gmail.com>
2021-07-12 17:28:25 +0100

    3

--- 3.py
+++ 3.py
@@ -3,3 +3,4 @@ largeur=float(input("donner la largeur"))
 surface=longueur*largeur
 perimetre=(longueur+largeur)*2
 print(f"surface est {surface} est les perimetre est {perimetre}")
+n=input("wait")
