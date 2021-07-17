d32fb1ef23167ac3aa9fb343300b51f548a32330
elhajuojy <souirimehdi311@gmail.com>
2021-07-17 14:38:23 +0100

    12

--- 12.py
+++ 12.py
@@ -1,9 +1,9 @@
-a=int(input("please entre a"))
-b=int(input("please entre b"))
+a=int(input("please entre a :"))
+b=int(input("please entre b :"))
 
 
-if(a*b)>0:
+if a*b>0:
     print("a and b have same signe")
 else:
-    print("a and b didn't have same signe")
-    
+    print("a and b  have differnce signe")
+
