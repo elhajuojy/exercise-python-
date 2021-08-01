b24e801380cf29c74c30ebe4600f2013988c11b6
elhajuojy <souirimehdi311@gmail.com>
2021-08-01 14:39:24 +0100

    15 categorie des enfnat

---
+++ 15.py
@@ -0,0 +1,11 @@
+age=int(input("please Entre age of the child .."))
+if age<6:
+    print("baby")
+elif 6<=age<=7:
+    print("Poussin")
+elif  8<=age<=9:
+    print("Pupolle")   
+elif   10<=age<=11:
+    print("Minime") 
+elif age>11:
+    print("cadet")    
