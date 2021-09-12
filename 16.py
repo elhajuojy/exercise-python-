2b185da608dd77e2bf25a7acb6e5479439dd0819
elhajuojy <souirimehdi311@gmail.com>
2021-09-12 15:17:00 +0100

    16 Programme qui calcule et affiche la moyenne et la mention des notes

---
+++ 16.py
@@ -0,0 +1,16 @@
+Note1=float(input("please Entre Number note 1"))
+Note2=float(input("please Entre Number note 2"))
+Note3=float(input("please Entre Number note 3"))
+Moyenne=Note1+Note2+Note3/3
+if Moyenne>16 :
+    print("Tres bien")
+elif Moyenne>14 :
+    print("bien")
+elif Moyenne>12:
+    print("Assez bien")
+elif Moyenne>10:
+    print("passablr") 
+else:
+    print("Insuffisant")      
+
+
